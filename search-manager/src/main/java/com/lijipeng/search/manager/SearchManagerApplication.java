656bfac1779193f1b0feaf8b547c522e69f24475
lijipeng <qw123456>
2020-05-31 21:23:45 +0800

    11

---
+++ search-manager/src/main/java/com/lijipeng/search/manager/SearchManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lijipeng.search.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchManagerApplication.class, args);
+    }
+
+}
