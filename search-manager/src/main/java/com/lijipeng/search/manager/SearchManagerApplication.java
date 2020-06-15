078dc652c43d17c01232d29274b36ebf03d8b37a
lijipeng <840605726@qq.com>
2020-06-15 11:39:27 +0800

    111

--- search-manager/src/main/java/com/lijipeng/search/manager/SearchManagerApplication.java
+++ search-manager/src/main/java/com/lijipeng/search/manager/SearchManagerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SearchManagerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SearchManagerApplication.class, args);
     }
